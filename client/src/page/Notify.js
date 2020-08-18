e3629010e8d76e22a49c8e52e5c7538dff5a89aa
Lý Minh Nghĩa <lmn2461999@gmail.com>
2020-08-18 16:17:13 +0700

    update notify page client

--- client/src/page/Notify.js
+++ client/src/page/Notify.js
@@ -37,37 +37,49 @@ const Notify = () => {
     }, [])
     
     return (
-        <>
-            <h1 style={{marginBottom: 20}}>Danh sách các hợp đồng sắp hết hạn</h1>
+        <div>
+            <h1 className="headerNotify">Danh sách các hợp đồng sắp hết hạn</h1>
             <div className="site-card-wrapper">
                 <Row gutter={24}>
                     {notify.map((search, index) => (
                         <Col span={8}>
                             <Card 
-                                title={search.main_cooperations[0].main_cooperation} 
+                                title={
+                                    <div className="headerCooperation">
+                                        {search.main_cooperations[0].main_cooperation}
+                                    </div>
+                                } 
                                 bordered={true} 
                                 type="inner"
                                 extra={<Link to={{pathname:`/cooperation-edit/${search.id}`}}>
-                                    <Button style={{ backgroundColor: 'white', color: '#1890ff', whiteSpace: 'inherit', height: 30, width: '50px !important' }}>
-                                        Gia hạn
+                                    <Button style={{ 
+                                        fontFamily: "Trocchi", 
+                                        borderWidth: 2, 
+                                        borderRadius: 20, 
+                                        backgroundColor: 'white', 
+                                        color: '#253b80', 
+                                        whiteSpace: 'inherit', 
+                                        height: 30, 
+                                        width: '50px !important' }}>
+                                            Gia hạn
                                     </Button>
                                 </Link>}
                                 
-                                style={{marginBottom: 20}}
+                                style={{marginBottom: 20, borderRadius: 20, borderStyle: "ridge", borderColor: "#253b80", borderWidth: 5}}
                             >
                                 <div>
-                                    <h4>Công ty</h4>
-                                    <p>{search.partner.name}</p>
+                                    <h4 className="headerCooperation">Đối tác</h4>
+                                    <p className="headerCooperation">{search.partner.name}</p>
                                 </div>
                                 <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
-                                    <h4>Còn lại</h4>
-                                    <h5 style={{color: '#4f8db3'}}>{moment(search.expiry_date).diff(Date.now(), 'day') + 1} ngày</h5>
+                                    <h4 style={{color: '#253b80'}}>Còn lại</h4>
+                                    <h5 style={{color: '#253b80'}}>{moment(search.expiry_date).diff(Date.now(), 'day') + 1} ngày</h5>
                                 </div>
                             </Card>
                         </Col>))}
                 </Row>
             </div>
-        </>
+        </div>
     )
 }
 
